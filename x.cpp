5c28b5d43e764dd2fe5ddd508c2af6eb90b388fa
user <agsci2017@gmail.com>
2017-12-18 16:19:38 +0300

    clean x.cpp

--- x.cpp
+++ x.cpp
@@ -2,23 +2,6 @@
 
 #define VIDEO "v.mp4"
 
-#define CONCAT_(x,y) x##y
-#define CONCAT(x,y) CONCAT_(x,y)
-
-#define RECTS(X)  \
-    for(int CONCAT(tmpvar, __LINE__)=0;CONCAT(tmpvar, __LINE__)<X.size();CONCAT(tmpvar, __LINE__)++){ rectangle( frame, X[CONCAT(tmpvar, __LINE__)], Scalar( 255, 0, 200 ), 2, 1 ); }
-
-
-#define SCALE1ARG(DIM) {resize(frame, frame, Size(), DIM, DIM, INTER_NEAREST);}
-#define SCALE2ARG(FRAME,DIM) {resize(FRAME, FRAME, Size(), DIM, DIM, INTER_NEAREST);}
-
-#define GET_3TH_ARG(arg1, arg2, arg3, ...) arg3
-#define MACRO_CHOOSER(...) \
-    GET_3TH_ARG(__VA_ARGS__, SCALE2ARG, SCALE1ARG, )
-
-#define SCALE(...) MACRO_CHOOSER(__VA_ARGS__)(__VA_ARGS__)
-
-
 /*
 RECTS(X)
 SCALE(DIM)
@@ -26,44 +9,12 @@ SCALE(FRAME,DIM)
 */
 
 
-
-string rand_str() {
-	
-	std::ostringstream oss;
-	oss << std::hex << (256*256*256+(rand() % (256*256*256)));
-	oss << std::hex << (256*256*256+(rand() % (256*256*256)));
-	oss << std::hex << (256*256*256+(rand() % (256*256*256)));
-	oss << std::hex << (256*256*256+(rand() % (256*256*256)));
-	return oss.str();
-}
-
-
-CascadeClassifier casc;
-vector<Rect2i> cars;
-
-
-
-
-
-
 START{
-	
-	namedWindow("img",1);  
-	//setMouseCallback("img", click, NULL);
-	
-	
-	casc.load("cascade.xml");
 	
 }
 
 LOOP{
 	SCALE(0.5)
-	
-	casc.detectMultiScale(frame, cars, 5, 5, 0,
-			cv::Size(18, 18), cv::Size(32, 32));
-	cout<<cars.size()<<endl;
-	
-	RECTS(cars)
 	
 	return OK;
 }
